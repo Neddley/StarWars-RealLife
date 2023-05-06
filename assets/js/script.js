1f2658a176b40bda75bf37d3fee5c3e4520c6b8f
Esteban Turrys <dr.turrys@gmail.com>
2023-05-06 14:18:40 -0400

    Se añaden bucles a secondary y tertiary

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ const index3 = document.getElementById("index3");
 const primary = document.getElementById("primary");
 const secondary = document.getElementById("secondary");
 const tertiary = document.getElementById("tertiary");
-const mainChars = document.querySelector("#main-chars")
+const mainChars = document.querySelector("#main-chars");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 class Character {
   constructor(name, height, mass) {
@@ -15,22 +19,27 @@ class Character {
 }
 
 function reveal1() {
+
+  index1.removeAttribute("onmouseenter");
+
+  index1.classList.add("active");
+
   for (i = 1; i < 6; i++) {
     fetch("https://swapi.dev/api/people/" + i)
       .then((response) => response.json())
       .then(
-        (data) => (primaryChar = new Character(data.name, data.height, data.mass))
+        (data) =>
+          (primaryChar = new Character(data.name, data.height, data.mass))
       )
       .then((primaryChar) => generator(primaryChar));
   }
 
-  function generator(data){
+  function generator(data) {
+    let name = data.name;
+    let height = data.height;
+    let mass = data.mass;
 
-    let name = data.name
-    let height = data.height
-    let mass = data.mass
-
-    primary.innerHTML +=`
+    primary.innerHTML += `
     <div class="v1">
           <div class="circle"></div>
           <div class="texts">
@@ -38,23 +47,39 @@ function reveal1() {
             <p>Estatura: ${height} cm. Peso: ${mass} kg.</p>
           </div>
         </div>
-    `
+    `;
   }
 
-  index1.removeAttribute("onmouseenter");
-  if (primary.clientHeight > 300) {
-    index1.classList.add("active");
-  }
+  
 }
 
 function reveal2() {
   for (i = 6; i < 11; i++) {
     fetch("https://swapi.dev/api/people/" + i)
       .then((response) => response.json())
-      .then((data) => (secondaryChar = new Character(data.name, data.height, data.mass))
+      .then(
+        (data) =>
+          (primaryChar = new Character(data.name, data.height, data.mass))
       )
-      .then((secondaryChar) => generator(secondaryChar));
+      .then((primaryChar) => generator(primaryChar));
   }
+
+  function generator(data) {
+    let name = data.name;
+    let height = data.height;
+    let mass = data.mass;
+
+    secondary.innerHTML += `
+    <div class="v2">
+          <div class="circle"></div>
+          <div class="texts">
+            <h2>${name}</h2>
+            <p>Estatura: ${height} cm. Peso: ${mass} kg.</p>
+          </div>
+        </div>
+    `;
+  }
+
   index2.removeAttribute("onmouseenter");
   if (secondary.clientHeight > 300) {
     index2.classList.add("active");
@@ -65,10 +90,29 @@ function reveal3() {
   for (i = 11; i < 16; i++) {
     fetch("https://swapi.dev/api/people/" + i)
       .then((response) => response.json())
-      .then((data) => (tertiaryChar = new Character(data.name, data.height, data.mass))
+      .then(
+        (data) =>
+          (primaryChar = new Character(data.name, data.height, data.mass))
       )
-      .then((tertiaryChar) => generator(tertiaryChar));
+      .then((primaryChar) => generator(primaryChar));
   }
+
+  function generator(data) {
+    let name = data.name;
+    let height = data.height;
+    let mass = data.mass;
+
+    tertiary.innerHTML += `
+    <div class="v3">
+          <div class="circle"></div>
+          <div class="texts">
+            <h2>${name}</h2>
+            <p>Estatura: ${height} cm. Peso: ${mass} kg.</p>
+          </div>
+        </div>
+    `;
+  }
+
   index3.removeAttribute("onmouseenter");
   if (tertiary.clientHeight > 300) {
     index3.classList.add("active");
